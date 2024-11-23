3ea5c94e11bc6184b7ec31f65a3ee0ec40f89e6e
Paula Mari <paulaelenamari@gmail.com>
2024-11-23 09:38:28 -0300

    API actualizada

--- src/Servicios/axios.js
+++ src/Servicios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://labor3-d60e.restdb.io/rest/', // Base URL de la API de CoinGecko
+  baseURL: 'https://labor3-d60e.restdb.io/rest/', 
   timeout: 5000, // Tiempo de espera de 5 segundos
   headers: {'x-apikey': '64a2e9bc86d8c525a3ed8f63'}
 });
